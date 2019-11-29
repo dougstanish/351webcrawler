b10fa8fd41adb09a32cf5906ee9910554cd58568
Spencer Hill <spencernh77@gmail.com>
2019-11-29 10:56:39 -0500

    Add SQLite support with support for searching and printing. Might need prettying up of the printing.

--- Webcrawler.py
+++ Webcrawler.py
@@ -1,4 +1,7 @@
 import requests
+import sqlite3 as sql
+import sys
+from datetime import datetime
 from bs4 import BeautifulSoup, Tag
 
 
@@ -7,7 +10,8 @@ class Conference:
     def __init__(self):
         self.event = None
         self.name = None
-        self.when = None
+        self.start = None
+        self.end = None
         self.where = None
         self.deadline = None
 
@@ -19,8 +23,10 @@ class Conference:
         elif self.name is None:
             self.name = info
 
-        elif self.when is None:
-            self.when = info
+        elif self.start is None:
+            if len(info.split('-')) == 2:
+                self.start = datetime.strptime(info.split('-')[0].strip(), "%b %d, %Y")
+                self.end = datetime.strptime(info.split('-')[1].strip(), "%b %d, %Y")
 
         elif self.where is None:
             self.where = info
@@ -30,11 +36,17 @@ class Conference:
 
     def __str__(self):
 
-        return f'event = {self.event}\nname = {self.name}\nwhen = {self.when}\nwhere = {self.where}\ndeadline = ' \
+        return f'event = {self.event}\nname = {self.name}\nwhen = {self.start} - {self.end}\nwhere = {self.where}\ndeadline = ' \
             f'{self.deadline}\n'
 
 
-def main():
+connection = sql.connect('events.db')
+cursor = connection.cursor()
+
+
+def build_db():
+    connection.execute("DROP TABLE IF EXISTS Events")
+    connection.execute("CREATE TABLE Events(Event TEXT, Name TEXT, FirstDate DateTime, LastDate DateTime, Location TEXT, Deadline TEXT)");
 
     r = requests.get('http://www.wikicfp.com/cfp/call?conference=computer%20science').text
 
@@ -83,10 +95,37 @@ def main():
         iterator += 1
 
     for conf in remove_links:
+        connection.execute("INSERT INTO Events VALUES(?, ?, date(?), date(?), ?, ?)", (conf.event, conf.name, conf.start, conf.end, conf.where, conf.deadline))
+    connection.commit()
 
-        print(conf)
+def print_all(): 
+    cursor.execute('SELECT * FROM Events')
+    for evt in cursor.fetchall():
+        print(evt)
 
-    pass
+def search(year, month):
+    cursor.execute('SELECT * FROM Events WHERE strftime(\'%Y-%m\', FirstDate) = ? OR strftime(\'%Y-%m\', LastDate) = ?', (year + '-' + month, year + '-' + month))
+    for evt in cursor.fetchall():
+        print(evt)
+
+def main():
+
+    if len(sys.argv) > 1:
+        if sys.argv[1].lower() == 'dbmake':
+            build_db()
+        elif sys.argv[1].lower() == 'all':
+            print_all()
+        elif sys.argv[1].lower() == 'search':
+            if len(sys.argv) != 4:
+                print("Search requires a year and month argument.")
+            else:
+                search(sys.argv[2], sys.argv[3])
+        else:
+            print("Invalid argument: " + sys.argv[1])
+    else:
+        print('Requires one of: dbmake, all, or search')
+
+    connection.close()
 
 
 if __name__ == '__main__':
