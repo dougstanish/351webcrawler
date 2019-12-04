c16f2a5b3ef6f02d5af7b43c124f710787bc07d9
dsbookbug <dsbookbug@gmail.com>
2019-12-03 19:48:48 -0500

    Added comments

--- Webcrawler.py
+++ Webcrawler.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup, Tag
 
 
 class Conference:
+    """
+    Class used to hold the info of a conference before insertion into database
+    """
 
     def __init__(self):
         self.event = None
@@ -18,131 +21,160 @@ class Conference:
         self.deadline = None
 
     def add_info(self, info):
+        """
+        Function that takes a piece of info, and adds it to the appropriate parameter
+        :param info: A string that represents data for the conference
+        """
 
-        if self.event is None:
+        if self.event is None:  # If the event is not set, sets it to the info
             self.event = info
 
-        elif self.name is None:
+        elif self.name is None:  # If the event name is not set, sets it to the info
             self.name = info
 
-        elif self.start is None:
-            if len(info.split('-')) == 2:
+        elif self.start is None:  # If the event start is not set
+            if len(info.split('-')) == 2:  # Splits the date into the start and end dates, and sets them
                 self.start = datetime.strptime(info.split('-')[0].strip(), "%b %d, %Y")
                 self.end = datetime.strptime(info.split('-')[1].strip(), "%b %d, %Y")
-            else:
+            else:  # Otherwise, set both to N/A
                 self.start = 'N/A'
                 self.end = 'N/A'
 
-        elif self.where is None:
+        elif self.where is None:  # If the event location is not set, sets it to the info
             self.where = info
 
-        elif self.deadline is None:
+        elif self.deadline is None:  # If the event deadline is not set, sets it to the info
             self.deadline = info
 
+    @property
     def __str__(self):
+        """
+        Sets the string representation of a conference
+        :return: The string representing the conference
+        """
 
-        return f'event = {self.event}\nname = {self.name}\nwhen = {self.start} - {self.end}\nwhere = {self.where}\ndeadline = ' \
-            f'{self.deadline}\n'
+        # Returns a string that shows all fields from the conference class
+        return f'event = {self.event}\nname = {self.name}\nwhen = {self.start} - {self.end}\nwhere = {self.where}\n' \
+               f'deadline = {self.deadline}\n'
 
 
+# Connect to the database
 connection = sql.connect('events.db')
 cursor = connection.cursor()
 
 
 def build_db():
-    connection.execute("DROP TABLE IF EXISTS Events")
-    connection.execute("CREATE TABLE Events(Event TEXT, Name TEXT, FirstDate DateTime, LastDate DateTime, Location TEXT, Deadline TEXT)");
+    """
+    Function that builds a database using information retrieved from the WikiCFP web calender
+    """
 
-    parameters = {'page': '1'}
+    connection.execute("DROP TABLE IF EXISTS Events")  # Deletes any existing data
+    connection.execute("CREATE TABLE Events(Event TEXT, Name TEXT, FirstDate DateTime, LastDate DateTime, "
+                       "Location TEXT, Deadline TEXT)")  # Recreates table after deleting old table
 
-    for page_num in range(1, 6):
+    parameters = {'page': '1'}  # Sets parameters for get request
 
-        if page_num != 1:
-            time.sleep(5)
+    for page_num in range(1, 6):  # For the first 5 pages
 
-        parameters['page'] = page_num
+        if page_num != 1:  # If this is not the first request
+            time.sleep(5)  # Waits 5 seconds before continuing
 
+        parameters['page'] = page_num  # Sets the page number to get
+
+        # Uses the requests library to send a get request to retrieve the requested page of the calender
         r = requests.get('http://www.wikicfp.com/cfp/call?conference=computer%20science', params=parameters).text
 
-        # print(r)
+        soup = BeautifulSoup(r, 'lxml')  # Parses the webpage into lxml
 
-        soup = BeautifulSoup(r, 'lxml')
-        # print(soup.prettify())
-
-        # <table cellpadding="3" cellspacing="1" align="center" width="100%">
+        # Finds the start of the calandar using its unique table attributes, as it does not have a tag
         test = soup.find('table', {'cellpadding': '3', 'cellspacing': '1', 'align': 'center', 'width': '100%'})
 
+        data = test.find_all('td')  # Finds the table
 
-        data = test.find_all('td')
+        conferences = []  # List of conferences
 
-        # print(data)
-
-        remove_links = []
-
-        cur_info = Conference()
+        cur_info = Conference()  # Creates new empty conference to be populated
 
         iterator = 0
+        for line in data:  # For each line in the table
 
-        for line in data:
-
-            if iterator < 4:
+            if iterator < 4:  # Skips the first 4 lines, as it does not contain any useful data
                 iterator += 1
                 continue
 
-            # if type(line.contents[0]) == NavigableString or type(line.contents[0]) == NavigableString:
-            content = line.contents[0]
+            content = line.contents[0]  # Gets the content of the line
 
-            if type(content) == Tag:
+            if type(content) == Tag:  # If the line contains a html tag
 
-                if len(content.contents) == 0:
+                if len(content.contents) == 0:  # If the value in the tag is 0, skips line
                     continue
 
-                content = content.contents[0]
+                content = content.contents[0]  # Sets the content to be the content of the tag
 
-            cur_info.add_info(content)
+            cur_info.add_info(content)  # Adds the content to the current conference object
 
-            if (iterator - 3) % 5 == 0 and (iterator-4) != 0:
-                remove_links.append(cur_info)
+            if (iterator - 3) % 5 == 0 and (iterator-4) != 0:  # Every 5 lines is a full conference entry
 
-                cur_info = Conference()
+                conferences.append(cur_info)  # Adds conference info to the array
 
-            iterator += 1
+                cur_info = Conference()  # Sets current conference to a new conference
 
-        for conf in remove_links:
-            connection.execute("INSERT INTO Events VALUES(?, ?, date(?), date(?), ?, ?)", (conf.event, conf.name, conf.start, conf.end, conf.where, conf.deadline))
+            iterator += 1  # Increases iterator
+
+        for conf in conferences:  # For each conference
+
+            # Inserts conference into database
+            connection.execute("INSERT INTO Events VALUES(?, ?, date(?), date(?), ?, ?)",
+                               (conf.event, conf.name, conf.start, conf.end, conf.where, conf.deadline))
+
+    connection.commit()  # Commits the database insertions to the database
 
 
+def print_all():
+    """
+    Prints every conference info from the database
+    """
+    cursor.execute('SELECT * FROM Events')  # Selects every event
+    for evt in cursor.fetchall():  # For each selected event
+        print(evt)  # Prints event
 
-    connection.commit()
-
-def print_all(): 
-    cursor.execute('SELECT * FROM Events')
-    for evt in cursor.fetchall():
-        print(evt)
 
 def search(year, month):
-    cursor.execute('SELECT * FROM Events WHERE strftime(\'%Y-%m\', FirstDate) = ? OR strftime(\'%Y-%m\', LastDate) = ?', (year + '-' + month, year + '-' + month))
-    for evt in cursor.fetchall():
-        print(evt)
+    """
+    Searches for events within a given date range
+    :param year: The year to search for
+    :param month: The month to search for
+    """
+    # Selects all events where the start date or end date fall within the givin year and month
+    cursor.execute('SELECT * FROM Events WHERE '
+                   'strftime(\'%Y-%m\', FirstDate) = ? OR strftime(\'%Y-%m\', LastDate) = ?',
+                   (year + '-' + month, year + '-' + month))
+    for evt in cursor.fetchall():  # For each selected event
+        print(evt)  # Prints event
+
 
 def main():
+    """
+    Main class that reads in args, and either populates a database with conference events, or returns selected
+    conferences
+    """
 
-    if len(sys.argv) > 1:
-        if sys.argv[1].lower() == 'dbmake':
-            build_db()
-        elif sys.argv[1].lower() == 'all':
-            print_all()
-        elif sys.argv[1].lower() == 'search':
-            if len(sys.argv) != 4:
-                print("Search requires a year and month argument.")
+    if len(sys.argv) > 1:  # If there are more than 1 arguments
+        if sys.argv[1].lower() == 'dbmake':  # If the command is to build the database
+            build_db()  # Builds the database
+        elif sys.argv[1].lower() == 'all':  # If the command is to print all events
+            print_all()  # Prints all events
+        elif sys.argv[1].lower() == 'search':  # If the argument is to search for a given month
+            if len(sys.argv) != 4:  # If there are not 4 args
+                print("Search requires a year and month argument.")  # Displays error message
             else:
-                search(sys.argv[2], sys.argv[3])
+                search(sys.argv[2], sys.argv[3])  # Searches for the given month and year
         else:
-            print("Invalid argument: " + sys.argv[1])
+            print("Invalid argument: " + sys.argv[1])  # Prints error message
     else:
-        print('Requires one of: dbmake, all, or search')
+        print('Requires one of: dbmake, all, or search')  # Prints error message
 
-    connection.close()
+    connection.close()  # Closes the connection
 
 
 if __name__ == '__main__':
